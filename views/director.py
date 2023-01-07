e3da48fecb256c8ff340177cb221a51e402a6594
gamer <denishel@mail.ru>
2023-01-07 18:30:46 +0500

    1

---
+++ views/director.py
@@ -0,0 +1,18 @@
+from flask_restx import Resource, Namespace
+
+from dao.model.director import DirectorSchema
+from implemented import director_service
+
+directors_ns = Namespace("directors")
+
+@directors_ns.route("/")
+class DirectorsView(Resource):
+    def get(self):
+        directors = director_service.get_all()
+        return DirectorSchema(many=True).dump(directors), 200
+
+@directors_ns.route("/<int:bid")
+class DirectorView(Resource):
+    def get(self, bid):
+        directors = director_service.get_one(bid)
+        return DirectorSchema().dump(directors), 200
